0a94146ec0bb90f8c59e55fcc426a7adc110c636
Diyorbektuit <d.xazratqulov.tuit.uz>
2024-05-15 22:09:00 +0500

    second commit

--- core/settings.py
+++ core/settings.py
@@ -80,23 +80,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Web',
-        'USER': 'postgres',
-        'PASSWORD': str(os.getenv('POSTGRES_PASSWORD')),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
