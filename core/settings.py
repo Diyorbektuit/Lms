b98e2e2f321f8ef59b52e6bf4434a0909a9d6806
Diyorbektuit <d.xazratqulov.tuit.uz>
2024-06-01 18:40:01 +0500

    gitignore

--- core/settings.py
+++ core/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w&9r+0ro^hpy1jm*$zl3y&ymu)%1c+8j@*m12brv9g)tc8uf#3'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
